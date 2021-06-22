111855d067d2e8dd923feee445a2eb7f8cf0e099
Shadrach John Kamata <shadrachkamata@gmail.com>
2021-06-22 09:58:12 -0700

    functions

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * main - check the code for ALX School students,
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char c;
+char phrase[] = "Holberton";
+for (c = 0; c < 9; c++)
+_putchar(phrase[c]);
+_putchar('\n');
+return (0);
+}
