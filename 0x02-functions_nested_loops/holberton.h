b16d5a7fe4ca0ee27380fac90a3f4eb49aff564a
Shadrach John Kamata <shadrachkamata@gmail.com>
2021-06-23 13:49:36 -0700

    functions

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,6 +1,24 @@
-#ifndef HOLBERTON_H_INCLUDED
-#define HOLBERTON_H_INCLUDED
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 
-int _putchar(char);
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ * */
 
-#endif
+int _putchar(char c);
+int print_alphabet(void);
+int print alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif /*_HOLBERTON_H_ */
