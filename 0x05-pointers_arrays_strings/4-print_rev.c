758bcb6700d2de275e92c288dc4296d6de381fea
Shadrach Kamata <shadrachkamata@gmail.com>
2021-06-28 11:38:53 -0700

    pointer and array

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,14 +8,14 @@
  */
 void print_rev(char *s)
 {
-	int counter , strenght;
+	int counter, strLength;
 
 	counter = 0;
-	while (*(s + counter) != "\0')
+	while (*(s + counter) != '\0')
 		counter++;
 	strLength = counter;
 	counter = 0;
 	while (*(s + counter) != '\0')
-		_putchar(*(s +(strLength - (counter++ + 1))));
+		_putchar(*(s + (strLength - (counter++ + 1))));
 	_putchar('\n')
 }
