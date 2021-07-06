63341692a648a58031652697d08ae4bdfb4f111b
Shadrach Kamata <shadrachkamata@gmail.com>
2021-07-06 03:35:52 -0700

    even pointers

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,21 +1,23 @@
-#include "holberton.h"
-
+#include <stdio.h>
 /**
- * _strchr - locates a character in a string.
- * @s: the string to search through.
- * @c: the character to be searched for.
+ * _strchr - finds if char occurs in string
  *
- * Return: a pointer to the first occurrence of c in s.
+ * @s: string to check
+ * @c: character to find in s
+ *
+ * Return: pointer to first instance of c, otherwise NULL
  */
 char *_strchr(char *s, char c)
 {
-	int i = 0;
+int i = 0;
 
-	while (s[i] != '\0')
-	{
-		if (s[i] == c)
-			return (&s[i]);
-		i++;
-	}
-	return (0);
+while (*(s + i))
+{
+if (*(s + i) == c)
+return (s + i);
+i++;
+}
+if (*(s + i) == c)
+return (s + i);
+return (NULL);
 }
