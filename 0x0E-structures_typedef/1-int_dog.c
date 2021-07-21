1b507c912b77d26915713b5480b973760ad4062f
Shadrach Kamata <shadrachkamata@gmail.com>
2021-07-21 04:59:35 -0700

    Bug fix

--- 0x0E-structures_typedef/1-int_dog.c
+++ 0x0E-structures_typedef/1-int_dog.c
@@ -1,18 +1,21 @@
 #include "dog.h"
 
+#define NULL (void *)0
+
 /**
  * init_dog - initialize a variable of type struct dog
- * @d: pointer to struct dog
- * @name: name of dog
- * @age: age of dog
- * @owner: owner's name
+ * @d: a pointer to struct dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the name of the dog's owner
+ *
+ * Return: nothing
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-
-if (!d)
-return;
-d->name = name;
-d->age = age;
-d->owner = owner;
+	if (d == NULL)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
