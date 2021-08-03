b33559936e6ca736d7ec71ac23001c3fd426697b
Shadrach Kamata <shadrachkamata@gmail.com>
2021-08-03 02:58:40 -0700

    hot fix

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,15 +1,23 @@
 #include "lists.h"
+
 /**
- * free_listint - frees a linked list
- * @head: head of list
- *
- * Return: always void
+ * free_listint - frees a listint_t list
+ * @head: head node;
  */
+
 void free_listint(listint_t *head)
 {
-while (head)
-{
-free(head);
-head = head->next;
-}
+	listint_t *tmp_node;
+
+	if (head == NULL)
+	{
+		return;
+	}
+	while (head != NULL)
+	{
+		tmp_node = head;
+		head = head->next;
+		free(tmp_node);
+	}
+	head = NULL;
 }
